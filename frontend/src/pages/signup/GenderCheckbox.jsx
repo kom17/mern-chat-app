5a09509b8b5477bfec7266f274f7b05186a1ba5e
keshriom <omkumarkeshri18@gmail.com>
2024-02-20 22:54:09 +0530

    4th part completed-socket

--- frontend/src/pages/signup/GenderCheckbox.jsx
+++ frontend/src/pages/signup/GenderCheckbox.jsx
@@ -1,23 +1,32 @@
-const GenderCheckbox = () => {
+const GenderCheckbox = ({onCheckboxChange,selectedGender}) => {
   return (
     <div className='flex'>
         <div className='form-control'>
-            <label className={`label gap-2 cursro-pointer`}>
+            <label className={`label gap-2 cursro-pointer ${selectedGender === "male" ? "selectec" : ""}`}>
                 <span className='label-text'>Male</span>
-                <input type='checkbox' className='checkbox border-slate-900'/>
+                <input
+                    type='checkbox' className='checkbox border-slate-900'
+                    checked ={selectedGender === "male"}
+                    onChange={() => onCheckboxChange("male")}
+                />
+
             </label>
         </div>
 
         <div className='form-control'>
-            <label className={`label gap-2 cursro-pointer`}>
+            <label className={`label gap-2 cursro-pointer  ${selectedGender === "female" ? "selectec" : ""}`}>
                 <span className='label-text'>Female</span>
-                <input type='checkbox' className='checkbox border-slate-900'/>
+                <input
+                    type='checkbox' className='checkbox border-slate-900'
+                    checked ={selectedGender === "female"}
+                    onChange={() => onCheckboxChange("female")}
+                />
             </label>
         </div>
       
     </div>
-  )
-}
+  );
+};
 
 export default GenderCheckbox;
 
